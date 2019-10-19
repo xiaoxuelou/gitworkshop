a2522a34597748e58fc5b9e429580e888d4e9345
Xiaoxue Lou <louxiaoxue@gmail.com>
2019-10-19 02:31:36 -0400

    Initial commit

---
+++ op.py
@@ -0,0 +1,10 @@
+def add(a,b):
+    return a+b
+def subtract(a,b):
+    return a-b
+def multiple(a,b):
+    return a*b
+def factorial(0):
+    if a==0:
+        return 1
+    return a* fractorial(a)
